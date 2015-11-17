95e60a7d2b7ee1f946426ef2e6ce2695899e6b74
hardwarefresser <eric@blastofeternity.de>
2015-11-17 17:24:28 +0100

    eschreibung für Ihre Änderungen ein. Zeilen,

--- flagger.py
+++ flagger.py
@@ -2,12 +2,8 @@ import telnetlib
 import time
 import MySQLdb as mdb
 
-##session = telnetlib.Telnet("127.0.0.1", 9999, 999)
-#session.write("flag{324aaaf64e72c3a8da3b4e41b2a8db05dae83ae8}")
-#print session.read_until("\n")
-
 def updateFlag(flag, state):
-	con = mdb.connect("172.17.0.13", "root", "toor", "flagDB")#open DB
+	con = mdb.connect("172.17.0.13", "root", "toor", "flagDB")		#open DB
 	cur = con.cursor()
 	print "willdosomething"
 	query="UPDATE flags SET state=\""+state+"\" where flag=\""+flag+"\";"	
@@ -16,13 +12,12 @@ def updateFlag(flag, state):
 	con.close()
 
 def extractFlags(state):
-	con = mdb.connect("172.17.0.13", "root", "toor", "flagDB")#open DB
-	cur = con.cursor()				#create cursor
-	query="SELECT flag from flags WHERE state=\""+state+"\";"	#SQl query
-	cur.execute(query)	#execute query
-	results = cur.fetchall()#fetch all results
-	flaglist = [val for sublist in results for val in sublist]#flatten list
-	#print flaglist	#print results
+	con = mdb.connect("172.17.0.13", "root", "toor", "flagDB")		#connect to DB
+	cur = con.cursor()							#create cursor
+	query="SELECT flag from flags WHERE state=\""+state+"\";"		#SQl query
+	cur.execute(query)							#execute query
+	results = cur.fetchall()						#fetch all results
+	flaglist = [val for sublist in results for val in sublist]		#flatten list
 	con.close()
 	return flaglist
 
@@ -31,9 +26,9 @@ def runLikeHell(flaglist):
 	session = telnetlib.Telnet("127.0.0.1", 9999, 999)
 	print session.read_until("\n")	
 	for flag in flaglist:
-		session.write(flag)		#write session to port
-		time.sleep(0.25)		#wait a bit
-		answer=session.read_until("\n")	#print message
+		session.write(flag)						#send flag to port
+		time.sleep(0.25)						#wait a bit
+		answer=session.read_until("\n")					#print message
 		print answer
 		
 		if "Accepted" in answer:
@@ -45,8 +40,10 @@ def runLikeHell(flaglist):
 		elif "your own" in answer:
 			updateFlag(flag, "own")
 		
-		
-
-flaglist=extractFlags("new")
-print flaglist
-runLikeHell(flaglist)		
+def main():
+	flaglist=extractFlags("new")
+	print flaglist
+	runLikeHell(flaglist)		
+	
+if __name__ == "__main__":
+	main()		
